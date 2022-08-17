66af004a4dd3bff46bda9789db80bae47f97b5a0
Dalengo117 <108893820+Dalengo117@users.noreply.github.com>
2022-08-17 11:10:02 -0500

    Update conexion.php

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -13,4 +13,25 @@ if($conexion){
     echo "<script>console.log('No se pudo conectar con la base de datos' );</script>";
 }
 
+//Para las graficas
+class conexion{
+    private $servidor;
+    private $usuario;
+    private $contrasena;
+    private $basedatos;
+    public $conexion;
+    public function __construct(){
+        $this->servidor = "localhost";
+        $this->usuario = "root";
+        $this->contrasena = "";
+        $this->basedatos = "bd_grafico";
+    }
+    function conectar(){
+        $this->conexion = new mysqli($this->servidor,$this->usuario,$this->contrasena,$this->basedatos);
+        $this->conexion->set_charset("utf8");
+    }
+    function cerrar(){
+        $this->conexion->close();
+    }
+}
 ?>
